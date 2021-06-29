6eac962d01d5a54755a2dd9b393e1afcd5a5a2de
gkolman <gkolman17@gmail.com>
2021-06-29 15:24:31 -0700

    most recent

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Grid from './features/grid/Grid.js'
+
+function App() {
+  return (
+    <div className="App">
+    <Grid/>
+    </div>
+  );
+}
+
+export default App;
